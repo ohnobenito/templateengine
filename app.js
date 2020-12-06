09406373d783f52a12c71e486563b9fc8e23825f
Benito Gonzalez <ohnobenito@gmail.com>
2020-12-06 17:50:38 -0600

    Added questions with inquirer, including different q's for differnt roles

--- app.js
+++ app.js
@@ -13,6 +13,62 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+function generateTeam() {
+const employeeInformation = inquirer.prompt([
+    { 
+        type: "list",
+        name: "role",
+        message: "What is the role you would like created?",
+        choices: ["Manager", "Engineer", "Intern"]
+    },
+    { 
+        type: "input",
+        name: "name",
+        message: "What is the manager's name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the manager's ID number?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the manager's email address?"
+    },
+        
+])
+.then(answers => {
+    if (answers.role === "Manager") {
+    inquirer.prompt ([
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "What is the managers office number?"
+        }
+    ])    
+    } else if (answers.role === "Engineer") {
+        inquirer.prompt ([
+            {
+                type: "input",
+                name: "gitHub",
+                message: "What is the Engineer's GitHub username?"
+            }
+        ])
+    } else {
+        inquirer.prompt ([
+            {
+                type: "input",
+                name: "school",
+                message: "What school does the intern attend?"
+            }
+        ])
+    }
+})
+
+}
+
+generateTeam();
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
@@ -33,3 +89,29 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+
+/*inquirer
+  .prompt([
+    {
+      name: "wants_pizza",
+      type: "confirm",
+      message: "Do you want a free pizza?",
+    },
+    {
+      name: "confirm_answer",
+      type: "confirm",
+      message: "Are you sure?",
+      when: (answers) => answers.wants_pizza === false,
+    },
+  ])
+  .then((answers) => {
+    if (answers.wants_pizza) {
+      console.log("The user wants free pizza");
+    } else if (answers.confirm_answer) {
+      // the user definitely doesn't want pizza
+    } else {
+      // the user changed their mind
+      // run the function to ask this question again
+    }
+  });*/
